3c205463558f1cc0f35250f90d67259d7f953a82
ellisvelandia <eyis619@gmail.com>
2022-10-01 20:21:28 -0500

    tetris

--- js/script.js
+++ js/script.js
@@ -18,17 +18,32 @@ const BLOCKS = {
       [1, 0],
       [1, 1],
     ],
-    [],
-    [],
-    [],
+    [
+      [1, 2],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [1, 2],
+      [0, 1],
+      [2, 1],
+      [1, 1],
+    ],
+    [
+      [2, 1],
+      [1, 2],
+      [1, 0],
+      [1, 1],
+    ],
   ],
 };
 
 const movingItem = {
   type: "tree",
-  direction: 0,
+  direction: 3,
   top: 0,
-  left: 3,
+  left: 0,
 };
 
 init();
@@ -53,17 +68,81 @@ function prependNewLine() {
   playground.prepend(li);
 }
 
-function renderBlocks() {
+function renderBlocks(moveType) {
   const { type, direction, top, left } = tempMovingItem;
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove(type, "moving");
+    console.log(moving);
+  });
+
   BLOCKS[type][direction].forEach((block) => {
     const x = block[0] + left;
     const y = block[1] + top;
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    target.classList.add(type);
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      setTimeout(() => {
+        if (moveType === "top") {
+          seizeBlock();
+        }
+        renderBlocks();
+      }, 0);
+
+      // renderBlocks();
+    }
   });
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+}
+
+function seizeBlock() {
+  console.log("seize block");
+}
+
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  tempMovingItem[moveType] += amount;
+  renderBlocks();
+}
+
+function chageDirection() {
+  const direction = tempMovingItem.direction;
+  direction === 3
+    ? tempMovingItem.direction = 0
+    : tempMovingItem.direction += 1;
+  renderBlocks();
 }
 
 // event handling
-document.addEventListener("keydown", e=> {
-  console.log(e)
-})
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock("left", 1);
+      break;
+    case 37:
+      moveBlock("left", -1);
+      break;
+    case 40:
+      moveBlock("top", 1);
+      break;
+    case 38:
+      chageDirection();
+      break;
+    default:
+      break;
+  }
+  // console.log(e);
+});
